ec75146e432a61da587dd277ec3ffd88c74ef897
Phạm Quân <phamquan29hy@gmail.com>
2023-10-03 23:44:48 +0700

    Google Login

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -3,17 +3,39 @@ const router=express.Router();
 const passport=require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
-// passport.use(new GoogleStrategy({
-//     clientID: GOOGLE_CLIENT_ID,
-//     clientSecret: GOOGLE_CLIENT_SECRET,
-//     callbackURL: "http://www.example.com/auth/google/callback"
-//   },
-//   function(accessToken, refreshToken, profile, cb) {
-//     User.findOrCreate({ googleId: profile.id }, function (err, user) {
-//       return cb(err, user);
-//     });
-//   }
-// ));
+passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: process.env.GOOGLE_CALLBACK_URL
+  },
+  function(accessToken, refreshToken, profile, cb) {
+   
+    console.log(profile)
+  }
+));
+
+
+// Google Login Route
+router.get(
+    "/auth/google",
+    passport.authenticate("google", { scope: ["email", "profile"] })
+  );
+  
+// Retrieve user data
+router.get(
+    "/google/callback",
+    passport.authenticate("google", {
+        failureRedirect: "/login-failure",
+        successRedirect: "/dashboard",
+    })
+);
+
+// Route if something goes wrong
+router.get('/login-failure', (req, res) => {
+    res.send('Something went wrong...');
+  });
+  
+
 
 
 module.exports = router;
